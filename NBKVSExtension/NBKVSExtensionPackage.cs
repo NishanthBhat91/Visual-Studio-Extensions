e829a5a5b52fc06c97c90b9bb145f96868e5f5bd
NishanthBhat <nishant.sukrutha@gmail.com>
2024-08-13 01:02:28 +0530

    Initial Commit

---
+++ NBKVSExtension/NBKVSExtensionPackage.cs
@@ -0,0 +1,21 @@
+﻿using Community.VisualStudio.Toolkit;
+using Microsoft.VisualStudio.Shell;
+using System;
+using Task = System.Threading.Tasks.Task;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace NBKVSExtension
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.NBKVSExtensionString)]
+    public sealed class NBKVSExtensionPackage : ToolkitPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+    }
+}
---
+++ NBKVSExtension/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Resources;
+using NBKVSExtension;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NBK Visual Studio Extension")]
+[assembly: AssemblyDescription("Visual studio extension package used for project management")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NBK")]
+[assembly: AssemblyProduct("Visual Studio Extension used for project management")]
+[assembly: AssemblyCopyright("NBK")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: Guid("D35BE0FE-CEF1-4EF0-806E-A69063789635")]
+
+namespace System.Runtime.CompilerServices
+{
+    public class IsExternalInit { }
+}
